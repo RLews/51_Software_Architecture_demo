6943de7ddbcd6d4bd34b21cf0591ca16e2f9c7be
RLews <410670107@qq.com>
2019-08-24 16:35:51 +0800

    2019年8月24日
    1.更新静态检测报告

---
+++ 01_Project_Code/0102_51_polyspace/c51/Module_1/CP_Result/.settings/launchingCommand.bat
@@ -0,0 +1,2 @@
+@echo off
+"D:\MATLAB\R2018b\polyspace\bin\polyspace-code-prover-nodesktop.exe" -options-file "E:\git\51_Software_Architecture_demo\01_Project_Code\0102_51_polyspace\c51\Module_1\CP_Result\.settings\options_command.txt" -results-dir "E:\git\51_Software_Architecture_demo\01_Project_Code\0102_51_polyspace\c51\Module_1\CP_Result" %*
